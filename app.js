f0c99bc81f4fc400a1d9ede5c819e7ea5051c094
Shohel Kashyapa <shohelkashapa809@gmail.com>
2023-08-20 23:13:41 +0530

    add some css

---
+++ app.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
