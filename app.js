e8eae550bb7763cc13b7842bed463acdc2bdd176
Shohel Kashyapa <shohelkashapa809@gmail.com>
2023-08-21 14:59:06 +0530

    Update Slider HTML CSS and JS

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
-const products = [
-  {
-    id: 1,
-    title: "Air Force",
-    price: 119,
-    colors: [
-      {
-        code: "black",
-        img: "./img/air.png",
-      },
-      {
-        code: "darkblue",
-        img: "./img/air2.png",
-      },
-    ],
-  },
+menuItems.forEach((item,index)=>{
+  item.addEventListener("click", () => {
+    wrapper.computedStyleMap.transform = 'tranlateX(${-100 * index}vw)';
+  });
+});
+
+
